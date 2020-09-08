f154921ba88d14d517bec070ea7ae1d3aae284bc
Danny Montano <dsmontano12@gmail.com>
2020-09-08 18:24:51 -0400

    created showInventory function, properly installed packages

--- bamazonCustomers.js
+++ bamazonCustomers.js
@@ -20,3 +20,20 @@ connection.connect(function (err) {
     console.log("connected as id " + connection.threadId);
 });
 
+function showInventory() {
+    connection.query("SELECT * FROM Products", function (err, data) {
+        if (err) throw err;
+
+        console.log();
+        console.log("------------ Welcome To BAMAZON ------------");
+        console.log("-------------------------------------------------------")
+        for (var i = 0; i < data.length; i++) {
+            console.log("Item ID: " + data[i].item_id + " | " + "Product Name: " + data[i].product_name + " | " + "Department Name: " + data[i].department_name + " | " + "Price: " + data[i].price + " | " + "Stock Quantity: " + data[i].stock_quantity);
+            console.log("---------------------------------------------------");
+            console.log(showInventory);
+    }
+    })
+};
+
+
+
